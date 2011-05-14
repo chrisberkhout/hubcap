25698afeeccba5ac41da396f69984200bd0f5371
Chris Berkhout <chrisberkhout@gmail.com>
2011-05-14 20:19:10 +0800

    HubStats now loads participation data.

--- lib/hubcap/hub_stats.rb
+++ lib/hubcap/hub_stats.rb
@@ -3,7 +3,6 @@ require 'uri'
 require 'json'
 
 module Hubcap
-  
   class HubStats
     
     attr_accessor :repos
@@ -14,20 +13,32 @@ module Hubcap
       @auth_params = { 'login' => @username }
       @auth_params['token'] = opts[:token] if opts[:token]
 
-      repo_list_url = URI.parse("http://github.com/api/v2/json/repos/show/#{@username}")
+      repo_list_url = URI.parse("https://github.com/api/v2/json/repos/show/#{@username}")
       result = Net::HTTP.post_form(repo_list_url, @auth_params)
       
       repo_array = JSON.parse(result.body)["repositories"]
       @repos = repo_array.reduce({}) { |hash, repo| hash[repo["name"]] = repo; hash }
 
-      
-      # repo_participation_url = URI.parse("https://github.com/#{@username}/#{repo}/graphs/participation")
-      # result = Net::HTTP.post_form(repo_participation_url, @auth_params)
-      # result.body
-      # # process JSON to build on @repos object
+      @repos.each_pair do |repo_name, data|
+        participation_url = URI.parse("https://github.com/#{@username}/#{repo_name}/graphs/participation")
+        owner_commits = Net::HTTP.post_form(participation_url, @auth_params).body.split(/[\r\n]+/)[1]
+        owner_commits.length == (52 * 2) || raise("Bad participation data for #{repo_name}.")
+        @repos[repo_name]["participation"] = owner_commits.scan(/../).map{ |code| base64_to_int(code) }
+      end
 
     end
     
+    protected
+    
+    def base64_to_int(input)
+      table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!-"
+      result = 0
+      input.scan(/./).reverse.each_with_index do |digit, index|
+        multiplier = (index == 0) ? 1 : index * 64
+        result = result + (table.index(digit) * multiplier)
+      end
+      result
+    end
+
   end
-  
 end
--- spec/hubcap/hub_stats_spec.rb
+++ spec/hubcap/hub_stats_spec.rb
@@ -5,45 +5,57 @@ require 'ruby-debug'
 module Hubcap
   describe HubStats do
     
-    before do
-      @username = "chrisberkhout"
-      @token    = "ef57c2ff4b4a75a2727e90927ebf52eb"
+    before(:all) do
+      USERNAME   = "chrisberkhout"
+      TOKEN      = "ef57c2ff4b4a75a2727e90927ebf52eb"
+      REPO_NAMES = ["hubcap", "rag_deploy", "beeswax"]
       FakeWeb.allow_net_connect = false
       FakeWeb.register_uri(
-        :any, "http://github.com/api/v2/json/repos/show/#{@username}", 
+        :any, "http://github.com:443/api/v2/json/repos/show/#{USERNAME}", 
         :body => File.read("spec/resources/repo_list.json")
       )
+      REPO_NAMES.each do |repo_name|
+        FakeWeb.register_uri(
+          :any, "http://github.com:443/#{USERNAME}/#{repo_name}/graphs/participation", 
+          :body => File.read("spec/resources/participation_#{repo_name}.base64")
+        )
+      end
     end
 
     describe "#initialize" do
       it "should be happy if there is a username" do
-        lambda { HubStats.new(:username => @username) }.should_not raise_error
+        lambda { HubStats.new(:username => USERNAME) }.should_not raise_error
       end
       it "should raise an error if the username is not specified" do
         lambda { HubStats.new }.should raise_error
       end
       it "should use the api token if provided" do
-        HubStats.new(:username => @username, :token => @token)
-        FakeWeb.last_request.body.split("&").include?("token=#{@token}").should be_true
+        HubStats.new(:username => USERNAME, :token => TOKEN)
+        FakeWeb.last_request.body.split("&").include?("token=#{TOKEN}").should be_true
       end
     end
 
     describe "#repos" do
-      before do
-        @repos = HubStats.new(:username => @username, :token => @token).repos
+      before(:all) do
+        @repos = HubStats.new(:username => USERNAME, :token => TOKEN).repos
       end
       it "should return a list of repos" do
         @repos.class.should == Hash
         @repos.length.should == 3
-        @repos.keys.sort.should == ["hubcap", "rag_deploy", "beeswax"].sort
+        @repos.keys.sort.should == REPO_NAMES.sort
       end
       it "should include public and private repos" do
         privacy_values = @repos.values.map{ |r| r["private"] }
         privacy_values.include?(true).should be_true
         privacy_values.include?(false).should be_true
       end
-      it "should include repo participation data" do
-        pending
+      it "should include participation data for public repos" do
+        @repos["hubcap"]["participation"].length.should == 52
+        @repos["hubcap"]["participation"][-1].should == 3
+      end
+      it "should include participation data for private repos" do
+        @repos["beeswax"]["participation"].length.should == 52
+        @repos["beeswax"]["participation"][14].should == 1
       end
     end
   
