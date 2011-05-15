67f885c011f152aab20c379767290f11149460f9
Chris Berkhout <chrisberkhout@gmail.com>
2011-05-15 13:52:19 +0800

    Fixed sinatra routes to handle GET and POST on root url.

--- lib/hubcap/controller.rb
+++ lib/hubcap/controller.rb
@@ -1,19 +1,22 @@
 set :views, Proc.new { File.join(root, "hubcap/views") }
 
-get '/' do
-  
-  if params[:login].nil? || params[:login].empty?
+respond = lambda do
 
-    if params.length == 0
-      haml :index
-    else
-      redirect to('/')
+  # if request.env['REQUEST_METHOD'] == 'POST'; end
+
+  if params[:login].nil? || params[:login].empty?
+    
+    if params.length > 0
+      # warn that login needs to be filled out
     end
+    haml :index
     
   else
-    
+
     haml :report
-    
+
   end
-  
 end
+
+get  '/', &respond
+post '/', &respond
