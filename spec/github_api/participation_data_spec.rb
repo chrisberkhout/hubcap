e326c87aae58b95b4a348ba8e7062f15d4345e5a
Chris Berkhout <chrisberkhout@gmail.com>
2011-05-25 20:53:38 +0800

    Added examples to spec the behavior of the live github api (official and unofficial).

---
+++ spec/github_api/participation_data_spec.rb
@@ -0,0 +1,16 @@
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec'
+
+describe "GitHub API (unofficial)" do
+  include Capybara
+  
+  before :all do
+    # Capybara setup
+  end
+
+  it "should provide participation data" do
+    pending
+  end
+  
+end
