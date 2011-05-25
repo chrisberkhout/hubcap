5540d46d6b0140eefe0209ea0b8e0950c2221303
Chris Berkhout <chrisberkhout@gmail.com>
2011-05-25 21:05:31 +0800

    Removed pending specs that are now dealt with elsewhere.

--- spec/hubcap/git_hub_spec.rb
+++ spec/hubcap/git_hub_spec.rb
@@ -61,12 +61,12 @@ module Hubcap
         privacy_values.include?(false).should be_true
       end
       
-      it "should include participation data for public repos" do
+      it "should include correctly decoded participation data for public repos" do
         @repos_with_participation.select{ |r| r['name'] == "hubcap" }.first["participation"].length.should == 52
         @repos_with_participation.select{ |r| r['name'] == "hubcap" }.first["participation"][-1].should == 3
       end
       
-      it "should include participation data for private repos" do
+      it "should include correctly decoded participation data for private repos" do
         @repos_with_participation.select{ |r| r['name'] == "beeswax" }.first["participation"].length.should == 52
         @repos_with_participation.select{ |r| r['name'] == "beeswax" }.first["participation"][14].should == 1
       end
@@ -76,7 +76,6 @@ module Hubcap
     describe "#repos for drnic" do
       
       before(:all) do
-        
         FakeWeb.register_uri(
           :any, "https://github.com/api/v2/json/repos/show/drnic?page=1", 
           :body => File.read("spec/resources/repos_show_drnic_1.json"),
@@ -92,7 +91,6 @@ module Hubcap
           :any, %r|https\://github\.com/drnic/.*?/graphs/participation|, 
           :body => File.read("spec/resources/participation_drnic_all.base64")
         )
-        
       end
 
       it "should load a multi-page repo list" do
@@ -105,14 +103,6 @@ module Hubcap
 
       it "should get participation data for the 20 repos most recently pushed to" do
         GitHub.new(:login => "drnic").repos_with_participation.map{|r| r['name'] }.sort.should == ["FakeWeb.tmbundle", "em-synchrony", "sinatra-synchrony", "sinatra-synchrony-example", "rails_wizard.web", ".dotfiles", "redcar", "red-dirt-workers-tutorial", "svruby-awards", "queue_classic", "test_edge_rails", "magic_multi_connections", "ci_demo_app", "rubigen", "tabtab", "todolist-webinar", "todolist-app", "composite_primary_keys", "docrails", "grape_test_app"].sort
-      end
-
-      it "should return repos with participation without any repos that have no commits" do
-        pending
-      end
-      
-      it "should return repos with participation sorted by first committed then first finished" do
-        pending
       end
 
     end  
