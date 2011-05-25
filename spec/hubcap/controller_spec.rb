bdd32466ac91898a57021ff7477f630fe5d5329d
Chris Berkhout <chrisberkhout@gmail.com>
2011-05-25 21:03:13 +0800

    Added note about when to factor out some setup as a helper.

--- spec/hubcap/controller_spec.rb
+++ spec/hubcap/controller_spec.rb
@@ -1,6 +1,7 @@
 require 'hubcap'
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/rspec'
 
 # http://www.sinatrarb.com/testing.html
 
@@ -11,9 +12,22 @@ describe Hubcap do
     Capybara.app = Sinatra::Application.new
   end
 
-  it "should serve login page" do
+  it "should serve a form for entering github credentials" do
+    pending
     visit '/'
     page.should have_content('GitHub login')
+  end
+  
+  context "with bad github credentials" do
+    # at this point, factor out the fakeweb cases into a helper
+    it "should return a message" do
+      pending
+    end
+  end
+  context "with good github credentials" do
+    it "should render the report" do
+      pending
+    end
   end
   
 end
